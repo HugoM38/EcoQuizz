00af28bfac5b9248612514caf48267a90851e920
StrippoliJules <julesstrippoli@gmail.com>
2025-02-11 10:58:44 +0100

    feat: add singIn and singnUp

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,31 +1,22 @@
 import 'dart:convert';
-
-// import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:ecoquizz/utils/shared_prefs_manager.dart';
 
 class AuthService {
-  Future<Response> login(String email, String password) async {
-    //TODO: Uncomment this code when api is ready
+  Future<http.Response> login(String email, String password) async {
+    final String api = "http://localhost:5001/api/auth";
+    final Uri apiUrl = Uri.parse('$api/signin');
 
-    // String api = "http://localhost:3000";
-    // Uri apiUrl = Uri.parse('$api/login');
-
-    // final response = await http.post(
-    //   apiUrl,
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: jsonEncode({
-    //     'email': email,
-    //     'password': password,
-    //   }),
-    // );
-
-    final response = Response('''{
-  "session_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWYzZjQwZjQwZjQwZjQwZjQwZjQwZjQwIiwic2Vzc2lvbl90b2tlbiI6IjE2Mjg4NzYwNzIwNzIiLCJpYXQiOjE2Mjg4NzYwNzJ9.1",
-  "user_id": "5f3f40f40f40f40f40f40f40"
-}''', 200);
+    final http.Response response = await http.post(
+      apiUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
 
     final responseData = json.decode(response.body);
 
@@ -33,36 +24,29 @@ class AuthService {
       final String sessionToken = responseData['session_token'];
       final String userId = responseData['user_id'];
       await SharedPreferencesManager.loginUser(userId, sessionToken);
+    } else if (response.statusCode == 401) {
+      throw "Email ou mot de passe incorrect";
     } else {
-      if (response.statusCode == 401) {
-        throw "Email ou mot de passe incorrect";
-      }
+      throw "Erreur lors de la connexion: ${response.body}";
     }
 
     return response;
   }
 
-  Future<Response> signup(String email, String password) async {
-    //TODO: Uncomment this code when api is ready
+  Future<http.Response> signup(String email, String password) async {
+    final String api = "http://localhost:5001/api/auth";
+    final Uri apiUrl = Uri.parse('$api/signup');
 
-    // String api = "http://localhost:3000";
-    // Uri apiUrl = Uri.parse('$api/signup');
-
-    // final response = await http.post(
-    //   apiUrl,
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: jsonEncode({
-    //     'email': email,
-    //     'password': password,
-    //   }),
-    // );
-
-    final response = Response('''{
-  "session_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWYzZjQwZjQwZjQwZjQwZjQwZjQwZjQwIiwic2Vzc2lvbl90b2tlbiI6IjE2Mjg4NzYwNzIwNzIiLCJpYXQiOjE2Mjg4NzYwNzJ9.1",
-  "user_id": "5f3f40f40f40f40f40f40f40"
-}''', 201);
+    final http.Response response = await http.post(
+      apiUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
 
     final responseData = json.decode(response.body);
 
@@ -70,10 +54,10 @@ class AuthService {
       final String sessionToken = responseData['session_token'];
       final String userId = responseData['user_id'];
       await SharedPreferencesManager.loginUser(userId, sessionToken);
+    } else if (response.statusCode == 401) {
+      throw "Email ou mot de passe incorrect";
     } else {
-      if (response.statusCode == 401) {
-        throw "Email ou mot de passe incorrect";
-      }
+      throw "Erreur lors de l'inscription: ${response.body}";
     }
 
     return response;

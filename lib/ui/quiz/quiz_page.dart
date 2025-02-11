0401b4a12c028aefe57d97951b41338bc4ee1b4c
Hugo <pv.hugom@gmail.com>
2025-02-11 09:14:58 +0100

    feat: update style of quiz page

--- lib/ui/quiz/quiz_page.dart
+++ lib/ui/quiz/quiz_page.dart
@@ -1,5 +1,7 @@
 import 'package:ecoquizz/models/question.dart';
 import 'package:ecoquizz/ui/home/result_page.dart';
+import 'package:ecoquizz/ui/widgets/ecoquizz_appbar.dart';
+import 'package:ecoquizz/ui/widgets/ecoquizz_button.dart';
 import 'package:flutter/material.dart';
 
 class QuizPage extends StatefulWidget {
@@ -22,77 +24,111 @@ class _QuizPageState extends State<QuizPage> {
     Question currentQuestion = widget.questions[currentQuestionIndex];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Question ${currentQuestion.ordre}'),
+      appBar: EcoQuizzAppBar(
+        title: "Question ${currentQuestion.ordre}",
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              currentQuestion.question,
-              style: TextStyle(fontSize: 20.0),
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: currentQuestion.answers.length, // Ajout de itemCount
-                itemBuilder: (context, index) {
-                  return RadioListTile<int>(
-                    title: Text(currentQuestion.answers[index].answer),
-                    value: index,
-                    groupValue: selectedAnswerIndex,
-                    onChanged: isValidated
-                        ? null
-                        : (value) {
-                            setState(() {
-                              selectedAnswerIndex = value;
-                            });
-                          },
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                currentQuestion.question,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 30),
+              Column(
+                children: currentQuestion.answers.asMap().entries.map((entry) {
+                  int index = entry.key;
+                  var answer = entry.value;
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                      color: selectedAnswerIndex == index
+                          ? Colors.green.shade100
+                          : Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        color: selectedAnswerIndex == index
+                            ? Colors.green
+                            : Colors.grey.shade300,
+                        width: 1.5,
+                      ),
+                    ),
+                    child: RadioListTile<int>(
+                      title: Text(
+                        answer.answer,
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      value: index,
+                      groupValue: selectedAnswerIndex,
+                      activeColor: Colors.green,
+                      onChanged: isValidated
+                          ? null
+                          : (value) {
+                              setState(() {
+                                selectedAnswerIndex = value;
+                              });
+                            },
+                    ),
                   );
+                }).toList(),
+              ),
+              SizedBox(height: 20),
+              EcoQuizzButton(
+                title: "Valider",
+                isEnable: selectedAnswerIndex != null && !isValidated,
+                onPressed: () {
+                  setState(() {
+                    isValidated = true;
+                    totalImpact +=
+                        currentQuestion.answers[selectedAnswerIndex!].impact;
+                  });
                 },
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  onPressed: (selectedAnswerIndex != null && !isValidated)
-                      ? () {
-                          setState(() {
-                            isValidated = true;
-                            totalImpact += currentQuestion
-                                .answers[selectedAnswerIndex!].impact;
-                          });
-                        }
-                      : null,
-                  child: Text('Valider'),
-                ),
-                IconButton(
-                  icon: Icon(Icons.arrow_forward),
-                  onPressed: isValidated
-                      ? () {
-                          if (currentQuestionIndex <
-                              widget.questions.length - 1) {
-                            setState(() {
-                              currentQuestionIndex++;
-                              selectedAnswerIndex = null;
-                              isValidated = false;
-                            });
-                          } else {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      ResultPage(totalImpact: totalImpact)),
-                            );
-                          }
-                        }
-                      : null,
-                )
-              ],
-            ),
-          ],
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: isValidated
+          ? FloatingActionButton(
+              onPressed: () {
+                if (currentQuestionIndex < widget.questions.length - 1) {
+                  setState(() {
+                    currentQuestionIndex++;
+                    selectedAnswerIndex = null;
+                    isValidated = false;
+                  });
+                } else {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ResultPage(totalImpact: totalImpact),
+                    ),
+                  );
+                }
+              },
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              child: Icon(
+                Icons.arrow_forward,
+                size: 30,
+              ),
+            )
+          : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: LinearProgressIndicator(
+          value: (currentQuestionIndex + 1) / widget.questions.length,
+          backgroundColor: Colors.grey.shade300,
+          valueColor: AlwaysStoppedAnimation<Color>(
+            Theme.of(context).colorScheme.primary,
+          ),
         ),
       ),
     );
